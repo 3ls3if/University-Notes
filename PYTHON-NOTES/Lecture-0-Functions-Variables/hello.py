d2c71539d52ef41a502025d45169cec027deec2e
Rohan <rohan1das1@gmail.com>
2024-04-11 02:08:53 +0530

    Updated

---
+++ PYTHON-NOTES/Lecture-0-Functions-Variables/hello.py
@@ -0,0 +1,19 @@
+# Ask user for their name
+
+name = input("What's your name? ")
+
+# printing name variable in different ways
+
+print(f"Hello, {name}")
+
+print("Hello, "+name)
+
+print("Hello, %s" % name)
+
+print("Hello, {}".format(name))
+
+print("Hello, {0}".format(name))
+
+
+print ("Hello, ", end="")
+print (name)
