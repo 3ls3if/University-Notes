935e75d2f16b38bd28d5e0ff2eec5657d239d34d
Rohan <rohan1das1@gmail.com>
2024-04-14 14:24:21 +0530

    updated

--- PYTHON-NOTES/Lecture-2-Loops/list-of-dictionaries.py
+++ PYTHON-NOTES/Lecture-2-Loops/list-of-dictionaries.py
@@ -3,9 +3,9 @@ students = [
     {"name": "Hermione", "House": "Gryffindor", "patronus": "Otter"},
     {"name": "Harry", "House": "Gryffindor", "patronus": "Stag"},
     {"name": "Ron", "House": "Gryffindor", "patronus": "Jack Russell terrier"},
-    {"name": "Draco", "House": "Slytherin", "patrous": None}
+    {"name": "Draco", "House": "Slytherin", "patronus": None}
 
 ]
 
 for student in students:
-    print(student["name"])
+    print(student["name"], student["House"], student["patronus"], sep=", ")
